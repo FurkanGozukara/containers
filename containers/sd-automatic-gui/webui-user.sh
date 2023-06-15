326f18532c6f09ba6b2631f6c9ec57ec0e49f8a6
Furkan Gözükara <furkangozukara@gmail.com>
2023-06-15 12:49:51 +0300

    Update webui-user.sh

--- containers/sd-automatic-gui/webui-user.sh
+++ containers/sd-automatic-gui/webui-user.sh
@@ -10,7 +10,7 @@ install_dir="/workspace"
 #clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-export COMMANDLINE_ARGS="--opt-sdp-attention --port 3000 --listen --enable-insecure-extension-access"
+export COMMANDLINE_ARGS="--xformers --port 3000 --listen --enable-insecure-extension-access"
 #export XFORMERS_PACKAGE="xformers==0.0.17.dev447"
 
 # python3 executable
